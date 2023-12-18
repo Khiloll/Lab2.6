5c25ac12421f15960592d191a689032568980f98
Hilol <khilol2017@gmail.com>
2023-12-18 04:55:31 +0300

    программы

---
+++ код/task2.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    # Создание исходного словаря
+    original_dict = {1: "one", 2: "two", 3: "three"}
+
+    reversed_dict = {v: k for k, v in original_dict.items()}
+
+    # Вывод исходного и нового словарей
+    print("Исходный словарь:", original_dict)
+    print('Новый "обратный" словарь:', reversed_dict)
